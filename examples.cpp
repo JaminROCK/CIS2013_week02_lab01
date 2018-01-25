e48a4b57b92e1633e265f9fddf5d3186c633164e
Schogan <lrschanbacher@gmail.com>
2018-01-24 20:16:13 -0600

    End of class

--- examples.cpp
+++ examples.cpp
@@ -4,23 +4,43 @@ using namespace std;
 
 int main(){
 	//Defines a
-	int room_length = 5;
+	int room_length = 10;
 	int room_width = 10;
-	float room_radius = 10;
+	char question; 
+	//float room_radius = 10;
+	 bool keep_running = true;
+	 while(keep_running){
+		cout << "Enter the room length:" << endl;
+		cin >> room_length;
 	 
-	//Prints out stuff
-	cout << "Our first number is " << room_length << endl;
-	cout << "Our second number is " << room_width << endl;
-	cout << "If you add them you get " << room_length + room_width << endl;
-	cout << "Unless your room is a circle " <<
-		(room_radius * room_radius) * 3.14 << endl;
+		cout << "Enter the room width:" << endl;
+		cin >> room_width;
+		//Prints out stuff
+		//cout << "Our first number is " << room_length << endl;
+		//cout << "Our second number is " << room_width << endl;
+		//cout << "If you add them you get " << room_length + room_width << endl;
+		//cout << "Unless your room is a circle " <<
+		//(room_radius * room_radius) * 3.14 << endl;
+		
+		if (room_length == room_width){
+		cout << "Length and Width are the same" << endl;		
+		}
+		else{
+		cout << "Length and Width are not the same" << endl;
+		}
 	
-	/*this is how
-	to do a
-	multi-line comment*/
+		cout << "Do you want to keep running?" << endl;
+		cin >> question;
+		
+		if (question != 'y'){keep_running = false;}
+		/*this is how
+		to do a
+		multi-line comment*/
+	
+		// == != > < >= <= 
 	
 	
-	
-	return 0;
-	
+	 }
+	 return 0;
 }
+
