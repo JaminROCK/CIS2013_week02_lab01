2cbb1cbe8567744d6cb8aa3f5e0c3077dd207753
Schogan <lrschanbacher@gmail.com>
2018-01-24 19:43:45 -0600

    Ignore stuff

---
+++ examples.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	//Defines a
+	int room_length = 5;
+	int room_width = 10;
+	float room_radius = 10;
+	 
+	//Prints out stuff
+	cout << "Our first number is " << room_length << endl;
+	cout << "Our second number is " << room_width << endl;
+	cout << "If you add them you get " << room_length + room_width << endl;
+	cout << "Unless your room is a circle " <<
+		(room_radius * room_radius) * 3.14 << endl;
+	
+	/*this is how
+	to do a
+	multi-line comment*/
+	
+	
+	
+	return 0;
+	
+}
